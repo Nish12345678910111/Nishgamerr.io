828de65e4508a9a3a6a21bccd427a150fa056cb9
Nish12345678910111 <Nishkarshrana10@gmail.com>
2025-10-21 18:42:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,6 @@ self.addEventListener('push', event => {
   const data = event.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: 'https://nish12345678910111.github.io/Nishgamerr.io/icon.png' // Optional icon
+    icon: 'https://nish12345678910111.github.io/Nishgamerr.io/icon.png' // Optional
   });
 });
