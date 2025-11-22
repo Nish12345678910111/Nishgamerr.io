7fb284ef2feb575fd61f25839cce66cdc6c4017b
Nish12345678910111 <Nishkarshrana10@gmail.com>
2025-11-22 20:16:52 +0530

    Update and rename sw.js to service-worker.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// sw.js
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: 'https://nish12345678910111.github.io/Nishgamerr.io/icon.png'
-  });
-});
